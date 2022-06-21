3f8fe01effcb1730c227323f8de629dee5d04edb
Antônio <antonio.nogueiraneto@icloud.com>
2022-06-21 15:26:23 -0300

    tipos primitivos, tipagem dinamica e operacoes aritimeticas

---
+++ Fundamentos/bloco-04-javascript/primeiros-passos/tiposprimitivos.js
@@ -0,0 +1,27 @@
+/* tipos primitivos */
+
+let movie = "Avengers"; //string literal - atribuindo o valor de string
+
+let score = 10; //number literal - atribuindo inteiros ou numeros quebrados
+
+let isValid = true; // boolean - true para verdadeiro e false para falso ou 1 ou 0
+
+let name; // undefined - indefinida
+
+let color = null; // nulo - podendo definir posteriormente - redefinicao de valor
+
+/* tipagem dinamica é quando podemos mudar alguma variavel */
+
+
+/* operacoes aritimeticas - + - /divisao *multiplicacao **expodencial  */
+
+let salary = 3500;
+/* console.log(salary - salary);
+console.log(salary + salary);
+console.log(salary / salary);
+console.log(salary * salary);
+console.log(5 ** 2);
+ */
+salary++; /* adiciona mais 1, incremento  */
+salary--; /* decrementa  */
+console.log(salary);
