6465adcafc2c18282e4c5ccc56d4290c17dca514
Antônio <antonio.nogueiraneto@icloud.com>
2022-06-21 14:47:37 -0300

    termino de java script variavel

--- Fundamentos/bloco-04/primeiros-passos/scripts.js
+++ Fundamentos/bloco-04/primeiros-passos/scripts.js
@@ -1,5 +1,19 @@
 let age = 20;
-let name = "antonio"
+let name = "antonio";
+let Name = "toninho";
+/* let é uma variavel  */
+
+/* podemos reatribuir valores em variaveis tipo let */
+
+age = 31;
+/* caso uma variavel seja constate usamos o const */
+
+const local = "Betim";
+
+
+/* usamos o console.log para trazer a informacao gravada para o terminal */
 
 console.log(age);
 console.log(name);
+console.log(Name);
+console.log(local);
