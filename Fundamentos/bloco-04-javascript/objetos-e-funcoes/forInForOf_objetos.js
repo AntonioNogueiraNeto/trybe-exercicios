bbf39a26a30dc1646db8487749e0a8d23f153eac
Antônio <antonio.nogueiraneto@icloud.com>
2022-06-24 17:09:33 -0300

    exemplos para

---
+++ Fundamentos/bloco-04-javascript/objetos-e-funcoes/forInForOf_objetos.js
@@ -0,0 +1,23 @@
+//imprimir todos os números pares entre 0 e 100 usando FOR tradicional
+// for (let i = 2; i <= 100; i += 2 ){
+//           console.log(i)
+// } 
+
+
+
+//decompor uma string e gerar um array com suas palavras
+//let arquivoTexto = 'Rodrigo Fabiana Arthur Joussemar Pétala';
+//let arrayNomes = arquivoTexto.split(' ')
+//console.log(arrayNomes)
+
+
+//como Pegar o maior numero de um array de forma rápida
+
+//let numeros = [3, 7, 13, 9, 155, 45, 123, 3, 155]
+
+//let maiorNumero = Math.max.apply(null, numeros)
+//console.log(maiorNumero);
+
+
+
+//O for of que nos permita caminhar por todos elementos do array do primeiro até o ultimo sem nessa sequencia
