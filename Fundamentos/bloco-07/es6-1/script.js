67be356b1e5a179ea4557b1e071ee3ee8c5dbc21
Antônio <antonio.nogueiraneto@icloud.com>
2022-07-25 16:53:14 -0300

    adicionando

---
+++ Fundamentos/bloco-07/es6-1/script.js
@@ -0,0 +1,22 @@
+// function contaPalavras (frase) {
+//     return frase.split(' ').length
+// }
+// console.log(contaPalavras('Fala tribo, beleza? caralho'));
+
+//  const contadorPalavras = (frase) => frase.split(' ').length
+
+// console.log(contadorPalavras('Fala tribo, beleza? caralho'));
+
+
+// function objetoPessoa (nome, idade){
+//     return{
+//         nome: nome,
+//         idade: idade,
+//     }
+// }
+
+// console.log(objetoPessoa('antonio', 25));
+
+
+const multiplyByTwo = number => number * 2;
+console.log(multiplyByTwo(10));
